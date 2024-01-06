d883e5d6e3fa258b062e78a7d985eab16d526ecc
royporat <roy.porat@bloxroute.com>
2024-01-06 17:36:27 +0200

    pack

--- Web/src/components/layout/PlayerLayout/index.tsx
+++ Web/src/components/layout/PlayerLayout/index.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import PackOfCards from "../../cards/pack/PackOfCards";
 import Score from "../../actions/Score";
 import { PlayerLayoutProps } from "../../../models/types/props";
+import style from "./PlayerLayout.module.css"
 
 const PlayerLayout: React.FC<PlayerLayoutProps> = ({ player }) => {
     return (
-        <section>
+        <section className={style.playerLayoutContainer}>
             <PackOfCards player={player}/>
             <Score score={player.score} />
         </section>
