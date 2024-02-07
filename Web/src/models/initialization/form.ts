92b0f2d8403f1f8e080cd21788a73ef1772c3b4b
royporat <roy.porat@bloxroute.com>
2024-02-07 21:52:49 +0200

    filter ui

--- Web/src/models/initialization/form.ts
+++ Web/src/models/initialization/form.ts
@@ -1,19 +1,19 @@
 import { FilterInputs } from "../types/form";
 
 export const filterInputs: FilterInputs = {
-    year: {
-        id: "year",
-        placeholder: "Select Release Years",
-        label: "Filter By Release Year",
-    },
     genre: {
         id: "genre",
         placeholder: "All Genres",
-        label: "Filter By Genre",
+        label: "Genres",
     },
     language: {
         id: "language",
         placeholder: "Select Native Language",
-        label: "Filter By Language",
+        label: "Native Language",
+    },
+    year: {
+        id: "year",
+        placeholder: "Select Release Years",
+        label: "Release Year",
     },
 };
