5f95867ef3ca505921ddea42eb6c10710cd9d493
royporat <roy.porat@bloxroute.com>
2024-02-25 16:16:42 +0200

    quit btn

--- Web/src/components/actions/btn/QuitBtn/index.tsx
+++ Web/src/components/actions/btn/QuitBtn/index.tsx
@@ -2,10 +2,18 @@ import React from "react";
 import PrimaryIconBtn from "../../core/button/PrimaryIconBtn";
 import { TiHome } from "react-icons/ti";
 import { QuitBtnProps } from "../../../../models/types/props";
+import useClear from "../../../../hooks/useClear";
+import { useNavigate } from "react-router-dom";
+import path from "../../../../router/routePath.json";
 
 const QuitBtn: React.FC<QuitBtnProps> = ({ close }) => {
+    const { handleClear } = useClear();
+    const navigate = useNavigate();
+
     const handleQuit = () => {
         close();
+        handleClear();
+        navigate(path.land);
     };
     const title = (
         <div style={{ display: "flex", alignItems: "center", gap: 10 }}>
