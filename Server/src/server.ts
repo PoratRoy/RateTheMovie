914347e1cff8462df07ad6d9d8894ceed55c9aef
royporat <roy.porat@bloxroute.com>
2024-03-16 20:01:25 +0200

    deploy test

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -16,7 +16,7 @@ mongoose
     .then(() => {
         console.log("Connected to database");
         const httpServer = app
-            .listen(PORT, "localhost", function () {
+            .listen(PORT, "0.0.0.0", function () {
                 console.log(`Server is running on port ${PORT}.`);
             })
             .on("error", (err: any) => {
