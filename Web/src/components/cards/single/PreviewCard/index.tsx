c793eff019cbeec4e3e747c8390fed53eff082ad
royporat <roy.porat@bloxroute.com>
2024-03-24 16:07:49 +0200

    card

--- Web/src/components/cards/single/PreviewCard/index.tsx
+++ Web/src/components/cards/single/PreviewCard/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import Card from "../../core/Card";
-import CardView from "../../../view/CardView";
 import { placeholderCardType } from "../../../../models/types/card";
 import CardEventLayout from "../../../layout/CardEventLayout";
 import { PreviewCardProps } from "../../../../models/types/props/card";
@@ -8,19 +7,22 @@ import Movie from "../../core/Movie";
 import { cardAnimation_click } from "../../../../models/initialization/card";
 
 const PreviewCard: React.FC<PreviewCardProps> = ({ movie }) => {
-    const [openCardView, setOpenCardView] = useState<boolean>(false);
     const [openShadow, setOpenShadow] = useState<boolean>(false);
 
     return (
-        <React.Fragment>
-            <CardEventLayout setOpenCardView={setOpenCardView} setOpenShadow={setOpenShadow}>
-                <Card
-                    type={{ t: "Preview" } as placeholderCardType}
-                    front={<Movie isShadow={openShadow} size="large" movie={movie} actions={cardAnimation_click} />}
-                />
-            </CardEventLayout>
-            {openCardView ? <CardView movie={movie} close={() => setOpenCardView(false)} /> : null}
-        </React.Fragment>
+        <CardEventLayout movie={movie} setOpenShadow={setOpenShadow}>
+            <Card
+                type={{ t: "Preview" } as placeholderCardType}
+                front={
+                    <Movie
+                        isShadow={openShadow}
+                        size="large"
+                        movie={movie}
+                        actions={cardAnimation_click}
+                    />
+                }
+            />
+        </CardEventLayout>
     );
 };
 
