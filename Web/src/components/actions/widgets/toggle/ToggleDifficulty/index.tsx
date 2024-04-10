c0f1d7b334d17b561f43932e048f9fabf73b9036
royporat <roy.porat@bloxroute.com>
2024-04-10 12:37:36 +0300

    toggle difficulty

--- Web/src/components/actions/widgets/toggle/ToggleDifficulty/index.tsx
+++ Web/src/components/actions/widgets/toggle/ToggleDifficulty/index.tsx
@@ -1,13 +1,8 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import style from "./ToggleDifficulty.module.css";
 import { Difficulty } from "../../../../../models/types/union";
 import { SelectRadio } from "../../../../../models/types/select";
-import {
-    SECONDARY_COLOR,
-    SECONDARY_COLOR_FOCUS,
-    TEXT_COLOR,
-    TEXT_COLOR_OPACITY_REAL,
-} from "../../../../../style/root";
+import { TEXT_COLOR, TEXT_COLOR_OPACITY_REAL } from "../../../../../style/root";
 import { ToggleDifficultyProps } from "../../../../../models/types/props/input";
 import { FieldValues } from "react-hook-form";
 import { FormSetValue } from "../../../../../models/constant";
@@ -17,10 +12,15 @@ const ToggleDifficulty = <TInput extends FieldValues>({
     setValue,
 }: ToggleDifficultyProps<TInput>) => {
     const [difficulty, setDifficulty] = useState<Difficulty>("easy");
+    const [ofset, setOfset] = useState<number>(0);
 
-    useEffect(() => {
+    const onToggle = (ev: any) => {
+        const difficulty: Difficulty = ev && ev.target.id;
+        setDifficulty(difficulty);
         setValue(id, difficulty, FormSetValue);
-    }, [difficulty]);
+        //TODO: Fix this
+        setOfset(difficulty === "easy" ? 0 : difficulty === "medium" ? 1 : 2);
+    };
 
     const options: SelectRadio[] = [
         { value: "easy", label: "Easy" },
@@ -28,32 +28,43 @@ const ToggleDifficulty = <TInput extends FieldValues>({
         { value: "hard", label: "Hard" },
     ];
 
+    const eachSwitchWidth = 80;
+    const noOfSwitches = options.length || 2;
+    const switchWidth = noOfSwitches * eachSwitchWidth;
+    const labelWidth = `${eachSwitchWidth || switchWidth / noOfSwitches}px`;
+
+    const switches = options.map((option: SelectRadio) => {
+        const { value, label } = option;
+        const labelStyles = {
+            width: labelWidth,
+            color: difficulty === value ? TEXT_COLOR : TEXT_COLOR_OPACITY_REAL,
+        };
+
+        return (
+            <label
+                key={value}
+                id={value}
+                className={style.multiSwitchContent}
+                style={labelStyles}
+                onClick={onToggle}
+            >
+                {label}
+            </label>
+        );
+    });
+
     return (
-        <div className={style.radioDifficulty}>
-            {options.map((option: SelectRadio, i: number) => (
-                <label
-                    key={i}
-                    className={style.radioDifficultyLabel}
-                    htmlFor={`${option.value}${i}`}
-                    style={{
-                        color: difficulty === option.value ? TEXT_COLOR : TEXT_COLOR_OPACITY_REAL,
-                        backgroundColor:
-                            difficulty === option.value ? SECONDARY_COLOR : SECONDARY_COLOR_FOCUS,
-                        borderRadius: difficulty === option.value ? 50 : "none",
-                    }}
-                >
-                    <input
-                        id={`${option.value}${i}`}
-                        type="radio"
-                        name="map-switch"
-                        value={option.value}
-                        checked={difficulty === option.value}
-                        onChange={(e) => setDifficulty(e.target.value as Difficulty)}
-                        className={style.radioDifficultyInput}
-                    />
-                    {option.label}
-                </label>
-            ))}
+        <div className={style.multiSwitchContainer} style={{ width: `${switchWidth}px` }}>
+            {switches}
+            <span
+                className={style.multiSwitchHandle}
+                style={{
+                    width: labelWidth,
+                    left: `${ofset * eachSwitchWidth + 2}px`,
+                }}
+            >
+                {}
+            </span>
         </div>
     );
 };
