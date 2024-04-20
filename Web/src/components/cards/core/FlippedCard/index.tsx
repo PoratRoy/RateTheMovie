a657d97ecc36d8dd9ebfab2b6c69361af9248997
royporat <roy.porat@bloxroute.com>
2024-04-20 14:09:40 +0300

    test card

--- Web/src/components/cards/core/FlippedCard/index.tsx
+++ Web/src/components/cards/core/FlippedCard/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { FlippedCardProps } from "../../../../models/types/props/card";
 import style from "./FlippedCard.module.css";
-import { useAnimationContext } from "../../../../context/AnimationContext";
-import { motion } from "framer-motion";
-import { CardFace } from "../../../../models/enums/animation";
+// import { useAnimationContext } from "../../../../context/AnimationContext";
+// import { motion } from "framer-motion";
+// import { CardFace } from "../../../../models/enums/animation";
 import Img from "../Img";
 import Position from "../../shadow/Position";
 import CardLayout from "../../../layout/CardLayout";
@@ -12,7 +12,7 @@ const FlippedCard: React.FC<FlippedCardProps> = ({
     id,
     card,
     position,
-    front,
+    // front,
     back,
     onClick,
     size = "large",
@@ -21,23 +21,23 @@ const FlippedCard: React.FC<FlippedCardProps> = ({
         id: movieId,
         movie: { title, poster_path },
     } = card;
-    const { isFlipCard } = useAnimationContext();
+    // const { isFlipCard } = useAnimationContext();
 
     return (
         <CardLayout id={id} onClick={onClick} size={size}>
-            <motion.div
+            {/* <motion.div
                 className={style.cardInnerContainer}
                 initial={false}
                 animate={{ rotateY: isFlipCard || CardFace.BACK }}
                 transition={{ duration: 0.3, animationDiraction: "normal" }}
-            >
-                <div style={{ transform: "rotateY(180deg)" }}>
-                    <Img isShadow alt={title} src={poster_path} />
-                </div>
-                <div className={style.cardFront}>{front}</div>
-                <div className={style.cardBack}>{back}</div>
-                {position ? <Position id={movieId} title={title} position={position} /> : null}
-            </motion.div>
+            > */}
+            {/* <div style={{ transform: "rotateY(180deg)" }}> */}
+            <Img isShadow alt={title} src={poster_path} />
+            {/* </div> */}
+            {/* <div className={style.cardFront}>{front}</div> */}
+            <div className={style.cardBack}>{back}</div>
+            {position ? <Position id={movieId} title={title} position={position} /> : null}
+            {/* </motion.div> */}
         </CardLayout>
     );
 };
