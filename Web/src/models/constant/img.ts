ddeabf2428d8a9ef48fea3dad5b83dcdd2da94e9
royporat <roy.porat@bloxroute.com>
2024-04-20 13:57:36 +0300

    avatar imgs

--- Web/src/models/constant/img.ts
+++ Web/src/models/constant/img.ts
@@ -1,17 +1,28 @@
 //https://blurha.sh/
 export const POSTER_BLUR_IMG = "XEC?cc~BxBR#oxS*S~D,#j-;=?r?RRVYWBJDb_J;X5s%jqV^jKDhD%V[xuo#";
 
-export const AVATAR_BLUR_IMG_1 = "UWKx3hoeCAa$I:jZ%2jsD,WV%0ofpJbHV?ae"
-export const AVATAR_BLUR_IMG_2 = "UpIF7MWV5ea$NhoIkWa{E2XA-mjZNhjEs*j]"
-export const AVATAR_BLUR_IMG_3 = "UDKJ-#oy00RQ~7niIuX904Wq~9n$8{j[?Hay"
-export const AVATAR_BLUR_IMG_4 = "UOGJ]hxB0bIt4TWA?aoeBtWD=qt76CWY-okD"
-export const AVATAR_BLUR_IMG_5 = "UlH.:Qj[5JWDyGoeM~a#R+ays5ayEAa#tRj@"
-export const AVATAR_BLUR_IMG_6 = "URO0yRX8_TnjVCa{pJj]0Vah^voc{goLFxah"
-export const AVATAR_BLUR_IMG_7 = "UhP6#IkB?AkC~ffQN2ayvkazJToK%Ij@jGaz"
-export const AVATAR_BLUR_IMG_8 = "UFF6aKv*0eIXGlf*-okV0yWB~VtP0$WD^4nj"
-export const AVATAR_BLUR_IMG_9 = "USH_umae?^ozR%WVxuofOFj]rqWBx^oLnha}"
-export const AVATAR_BLUR_IMG_10 = "UAN+a-Rk-_tQ~pWV57n%T2xF}PSg6JkC=Hn$"
-
+export const AVATAR_BLUR_IMG_1 = "UWKx3hoeCAa$I:jZ%2jsD,WV%0ofpJbHV?ae";
+export const AVATAR_BLUR_IMG_2 = "UpIF7MWV5ea$NhoIkWa{E2XA-mjZNhjEs*j]";
+export const AVATAR_BLUR_IMG_3 = "UDKJ-#oy00RQ~7niIuX904Wq~9n$8{j[?Hay";
+export const AVATAR_BLUR_IMG_4 = "UOGJ]hxB0bIt4TWA?aoeBtWD=qt76CWY-okD";
+export const AVATAR_BLUR_IMG_5 = "UlH.:Qj[5JWDyGoeM~a#R+ays5ayEAa#tRj@";
+export const AVATAR_BLUR_IMG_6 = "URO0yRX8_TnjVCa{pJj]0Vah^voc{goLFxah";
+export const AVATAR_BLUR_IMG_7 = "UhP6#IkB?AkC~ffQN2ayvkazJToK%Ij@jGaz";
+export const AVATAR_BLUR_IMG_8 = "UFF6aKv*0eIXGlf*-okV0yWB~VtP0$WD^4nj";
+export const AVATAR_BLUR_IMG_9 = "USH_umae?^ozR%WVxuofOFj]rqWBx^oLnha}";
+export const AVATAR_BLUR_IMG_10 = "UAN+a-Rk-_tQ~pWV57n%T2xF}PSg6JkC=Hn$";
 
 //https://imagekit.io/
 export const POSTER_IMG = "https://ik.imagekit.io/df9g0ij1q/poster.png?updatedAt=1713607582596";
+
+export const AVATAR_IMG_1 = "https://ik.imagekit.io/df9g0ij1q/avater1.png?updatedAt=1713610101954";
+export const AVATAR_IMG_2 = "https://ik.imagekit.io/df9g0ij1q/avater2.png?updatedAt=1713610157119";
+export const AVATAR_IMG_3 = "https://ik.imagekit.io/df9g0ij1q/avater3.png?updatedAt=1713610151216";
+export const AVATAR_IMG_4 = "https://ik.imagekit.io/df9g0ij1q/avater4.png?updatedAt=1713610137532";
+export const AVATAR_IMG_5 = "https://ik.imagekit.io/df9g0ij1q/avater5.png?updatedAt=1713610094930";
+export const AVATAR_IMG_6 = "https://ik.imagekit.io/df9g0ij1q/avater6.png?updatedAt=1713610108708";
+export const AVATAR_IMG_7 = "https://ik.imagekit.io/df9g0ij1q/avater7.png?updatedAt=1713610115042";
+export const AVATAR_IMG_8 = "https://ik.imagekit.io/df9g0ij1q/avater8.png?updatedAt=1713610129194";
+export const AVATAR_IMG_9 = "https://ik.imagekit.io/df9g0ij1q/avater9.png?updatedAt=1713610122729";
+export const AVATAR_IMG_10 =
+    "https://ik.imagekit.io/df9g0ij1q/avater10.png?updatedAt=1713610166726";
--- Web/src/models/resources/avatars.ts
+++ Web/src/models/resources/avatars.ts
@@ -1,13 +1,3 @@
-import Avatar1 from "../../assets/avatars/avatar1.png";
-import Avatar2 from "../../assets/avatars/avatar2.png";
-import Avatar3 from "../../assets/avatars/avatar3.png";
-import Avatar4 from "../../assets/avatars/avatar4.png";
-import Avatar5 from "../../assets/avatars/avatar5.png";
-import Avatar6 from "../../assets/avatars/avatar6.png";
-import Avatar7 from "../../assets/avatars/avatar7.png";
-import Avatar8 from "../../assets/avatars/avatar8.png";
-import Avatar9 from "../../assets/avatars/avatar9.png";
-import Avatar10 from "../../assets/avatars/avatar10.png";
 import { Avatar } from "../types/player";
 import {
     AVATAR_BLUR_IMG_1,
@@ -20,17 +10,27 @@ import {
     AVATAR_BLUR_IMG_7,
     AVATAR_BLUR_IMG_8,
     AVATAR_BLUR_IMG_9,
+    AVATAR_IMG_1,
+    AVATAR_IMG_10,
+    AVATAR_IMG_2,
+    AVATAR_IMG_3,
+    AVATAR_IMG_4,
+    AVATAR_IMG_5,
+    AVATAR_IMG_6,
+    AVATAR_IMG_7,
+    AVATAR_IMG_8,
+    AVATAR_IMG_9,
 } from "../constant/img";
 
 export const Avatars: Avatar[] = [
-    { img: Avatar1, hash: AVATAR_BLUR_IMG_1 },
-    { img: Avatar2, hash: AVATAR_BLUR_IMG_2 },
-    { img: Avatar3, hash: AVATAR_BLUR_IMG_3 },
-    { img: Avatar4, hash: AVATAR_BLUR_IMG_4 },
-    { img: Avatar5, hash: AVATAR_BLUR_IMG_5 },
-    { img: Avatar6, hash: AVATAR_BLUR_IMG_6 },
-    { img: Avatar7, hash: AVATAR_BLUR_IMG_7 },
-    { img: Avatar8, hash: AVATAR_BLUR_IMG_8 },
-    { img: Avatar9, hash: AVATAR_BLUR_IMG_9 },
-    { img: Avatar10, hash: AVATAR_BLUR_IMG_10 },
+    { img: AVATAR_IMG_1, hash: AVATAR_BLUR_IMG_1 },
+    { img: AVATAR_IMG_2, hash: AVATAR_BLUR_IMG_2 },
+    { img: AVATAR_IMG_3, hash: AVATAR_BLUR_IMG_3 },
+    { img: AVATAR_IMG_4, hash: AVATAR_BLUR_IMG_4 },
+    { img: AVATAR_IMG_5, hash: AVATAR_BLUR_IMG_5 },
+    { img: AVATAR_IMG_6, hash: AVATAR_BLUR_IMG_6 },
+    { img: AVATAR_IMG_7, hash: AVATAR_BLUR_IMG_7 },
+    { img: AVATAR_IMG_8, hash: AVATAR_BLUR_IMG_8 },
+    { img: AVATAR_IMG_9, hash: AVATAR_BLUR_IMG_9 },
+    { img: AVATAR_IMG_10, hash: AVATAR_BLUR_IMG_10 },
 ];
